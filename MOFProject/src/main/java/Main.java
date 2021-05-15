54fef26bef83c99d965d7d50e5430a7665418237
Gabriel Augusto Arruda <gabriel.arruda@oktagon.com.br>
2021-05-15 14:30:11 -0300

    changed: ajusted lines

--- MOFProject/src/main/java/Main.java
+++ MOFProject/src/main/java/Main.java
@@ -3,22 +3,12 @@ import com.megahard.mofproject.control.DBContext;
 import com.megahard.mofproject.model.Pedido;
 import com.megahard.mofproject.utils.ListUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rafae
- */
 public class Main {
     public static void main(String[] args){
-        Pedido p = new Pedido();
         ListUtils.printList(DBContext.getInstance().getDbPedido());
         DBContext.getInstance().getDbPedido().add(new Pedido());
-        ListUtils.printList(DBContext.getInstance().getDbPedido());
-        
+        DBContext.getInstance().getDbPedido().add(new Pedido());
+        ListUtils.printList(DBContext.getInstance().getDbPedido());   
     }
 }
+
